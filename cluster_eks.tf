d9621490c737401cfd089128a609ee20f2cf30eb
aortega20 <aortega20>
2022-06-28 12:39:30 -0300

    se corrige el launch_template

--- cluster_eks.tf
+++ cluster_eks.tf
@@ -16,9 +16,10 @@ resource "aws_eks_node_group" "worker-node-group" {
   subnet_ids   = [aws_subnet.subnet-ms-az-a.id, aws_subnet.subnet-ms-az-b.id]
   instance_types = ["t3.xlarge"]
   remote_access {
-    source_security_groups_id = [aws_security_group.ms-nodes-sg.id]
+    source_security_group_ids = [aws_security_group.ms-nodes-sg.id]
   }
   launch_template {
+    version = aws_launch_template.default_version 
     name  = "MS-Worker-Node"
   }
    
